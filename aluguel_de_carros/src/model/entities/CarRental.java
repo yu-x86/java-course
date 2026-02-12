033dbec4278c88796cccbf4cbf5f303b2cfc5e40
yu-x86 <oyunoodev@gmail.com>
2026-02-12 11:18:03 -0300

    Add new excersises

---
+++ aluguel_de_carros/src/model/entities/CarRental.java
@@ -0,0 +1,54 @@
+package model.entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	private Vehicle model;
+	private Invoice invoice;
+	
+	public CarRental() {
+		
+	}
+
+	public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle model) {
+		this.start = start;
+		this.finish = finish;
+		this.model = model;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getFinish() {
+		return finish;
+	}
+
+	public void setFinish(LocalDateTime finish) {
+		this.finish = finish;
+	}
+
+	public Vehicle getModel() {
+		return model;
+	}
+
+	public void setModel(Vehicle model) {
+		this.model = model;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}	
+	
+	
+}
---
+++ aluguel_de_carros/src/model/services/BrazilTaxService.java
@@ -0,0 +1,11 @@
+package model.services;
+
+public class BrazilTaxService implements TaxService{
+	public double tax(double amount) {
+		if(amount <= 100) {
+			return amount * (20.0/100.0);
+		} else {
+			return amount * (15.0/100.0);
+		}
+	}
+}
